f57422728dd6ffa167e422f1b369672caad99f9a
trashpirate <nadinaoates@gmail.com>
2024-05-18 22:44:35 -0500

    adding balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from uuid import uuid4
 from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import (
     filters,
@@ -12,7 +13,7 @@ from telegram.ext import (
 
 
 from credentials import TOKEN, PORT, URL
-from reflections import calcReflections
+from reflections import calcReflections, getBalanceOf
 
 
 logging.basicConfig(
@@ -22,7 +23,16 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-
+def getReflections(address):
+        reflections = calcReflections(address)
+        text = 'Reflections: {:,.2f} EARN'.format(reflections)
+        return text
+        
+def getBalance(address):
+    balance = getBalanceOf(address)
+    text = 'Balance: {:,.2f} EARN'.format(balance)
+    return text
+        
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
@@ -44,9 +54,9 @@ async def reflections(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.args and len(context.args) == 1:
         address = context.args[0]
         if len(address) == 42 and address[:2] == "0x":
-            reflections = calcReflections(address)
+            reflect = getReflections(address)
             await context.bot.send_message(
-                chat_id=update.effective_chat.id, text='Reflections: {:,.2f} EARN'.format(reflections)
+                chat_id=update.effective_chat.id, text=reflect
             )
         else:
             await context.bot.send_message(
@@ -56,27 +66,29 @@ async def reflections(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         return
 
+
 async def inline_reflections(update: Update, context: ContextTypes.DEFAULT_TYPE):
     address = update.inline_query.query
     if not address:
         return
     if len(address) == 42 and address[:2] == "0x":
-        reflections = calcReflections(address)
-        text = 'Reflections: {:,.2f} EARN'.format(reflections)
-        results = []
-        results.append(
+        reflect = getReflections(address)
+        balance = getBalance(address)
+        results = [
             InlineQueryResultArticle(
-                id=address,
+                id=str(uuid4()),
                 title="Reflections",
-                input_message_content=InputTextMessageContent(text),
+                input_message_content=InputTextMessageContent(reflect),
+            ),
+            InlineQueryResultArticle(
+                id=str(uuid4()),
+                title="Balance",
+                input_message_content=InputTextMessageContent(balance),
             )
-        )
+        ]
         await context.bot.answer_inline_query(update.inline_query.id, results)
     else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Please enter a valid wallet address.",
-        )
+        return
         
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -98,7 +110,6 @@ if __name__ == "__main__":
     application.add_handler(reflections_handler)
     application.add_handler(inline_reflections_handler)
 
-    # application.run_polling(allowed_updates=Update.ALL_TYPES)
     if URL == "":
         application.run_polling(poll_interval=5)
     else:
